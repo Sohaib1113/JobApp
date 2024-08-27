a9fc94f1c6b7413e50ca1c29bfaed083662263f5
Sohaib Syed <101351817+Sohaib1113@users.noreply.github.com>
2024-08-27 12:30:15 +0530

    Update store.js

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,1 +1,13 @@
 
+import { configureStore } from '@reduxjs/toolkit';
+import jobsReducer from '../features/jobs/jobsSlice';
+import appliedJobsReducer from '../features/appliedJobs/appliedJobsSlice';
+
+const store = configureStore({
+  reducer: {
+    jobs: jobsReducer,
+    appliedJobs: appliedJobsReducer,
+  },
+});
+
+export default store;
